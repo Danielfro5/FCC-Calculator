b5d3ad3731c7d868562ffd953d100cfd59c20e7a
Danielfro5 <br3akcor3@yandex.com>
2017-05-24 13:46:16 +1000

    most functionality complete

--- main.js
+++ main.js
@@ -13,11 +13,27 @@ function opPress(key){
 }
 
 function equalsPress(){
+
   var ans = document.getElementById("txtBox").innerHTML;
-  ans = math.eval(ans);
-  document.getElementById("txtBox").innerHTML = ans;
+  if(ans.length>0){
+    ans = math.eval(ans);
+    document.getElementById("txtBox").innerHTML = ans;
+  }
+
 }
 
 function ACPress(){
   document.getElementById("txtBox").innerHTML = "";
 }
+
+function checkLength(){
+  for(i=0;i<2;){
+    var ans = document.getElementById("txtBox").innerHTML;
+    if(ans.length>22){
+      document.getElementById("txtBox").innerHTML = "";
+    }
+  }
+
+}
+
+//BUGS:AFTER = HIT, NEXT INPUT MUST BE OPERATOR OR RESET KEY //IF MORE THAN 22 IN LENGTH ERROR DIGIT
